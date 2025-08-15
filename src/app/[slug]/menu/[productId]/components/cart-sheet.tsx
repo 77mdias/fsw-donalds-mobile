eaf986e04e6d16f57624255f5df4ba2a161406b7
77mdias <jean.carlos3@outlook.com>
2025-08-15 17:31:10 -0300

    feat: add initial cart item component

--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -1,30 +1,22 @@
 import { useContext } from "react";
 
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
-import { formatCurrency } from "@/helpers/format-currency";
 
 import { CartContext } from "../../contexts/cart";
+import CartProductItem from "./cart-product-item";
 
 const CartSheet = () => {
   const { isOpen, toggleCart, products } = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-      <SheetContent>
+      <SheetContent className='w-[80%]'>
         <SheetHeader>
-          <SheetTitle>Sacola</SheetTitle>
+          <SheetTitle className="text-left mb-5">Sacola</SheetTitle>
         </SheetHeader>
-        <div className="flex flex-col gap-4">
+        <div className="py-5">
           {products.map((product) => (
-            <div key={product.id}>
-              <h3>{product.name}</h3>
-              <p>{formatCurrency(product.price)}</p>
-              <p>{product.quantity}</p>
-            </div>
+            <CartProductItem key={product.id} product={product} />
           ))}
-          <div className="flex items-center justify-between">
-            <p>Total</p>
-            <p>{formatCurrency(products.reduce((acc, product) => acc + product.price * product.quantity, 0))}</p>
-          </div>
         </div>
       </SheetContent>
     </Sheet>
