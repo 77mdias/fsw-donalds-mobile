a52234f01fdcd2f8b0e3c88b06da2799ae604c26
77mdias <jean.carlos3@outlook.com>
2025-08-15 17:54:58 -0300

    feat: ensure cart total is show

--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { formatCurrency } from "@/helpers/format-currency";
 
 import { CartContext } from "../../contexts/cart";
 import CartProductItem from "./cart-product-item";
@@ -13,10 +16,21 @@ const CartSheet = () => {
         <SheetHeader>
           <SheetTitle className="text-left mb-5">Sacola</SheetTitle>
         </SheetHeader>
-        <div className="py-5">
-          {products.map((product) => (
+        <div className="py-5 flex flex-col h-full">
+          <div className="flex-auto ">
+            {products.map((product) => (
             <CartProductItem key={product.id} product={product} />
           ))}
+          </div>
+          <Card className="mb-6">
+            <CardContent>
+              <div className="flex justify-between">
+                <p className="text-sm text-muted-foreground">Total</p>
+                <p className="text-sm font-semibold">{formatCurrency(products.reduce((acc, product) => acc + product.price * product.quantity, 0))}</p>
+              </div>
+            </CardContent>
+          </Card>
+          <Button className="w-full rounded-full mb-5">Finalizar compra</Button>
         </div>
       </SheetContent>
     </Sheet>
