a385508a2a9ab84086a9205d9eead86737c050a6
77mdias <jean.carlos3@outlook.com>
2025-08-15 17:37:36 -0300

    feat: ensure cart product quantity can be decresed and increase and remove product

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -11,6 +11,9 @@ export interface ICartContext {
   products: CartProduct[];
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
+  removeProduct: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -18,6 +21,9 @@ export const CartContext = createContext<ICartContext>({
   products: [],
   toggleCart: () => {},
   addProduct: () => {},
+  decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
+  removeProduct: () => {},
 });
 
 export const CartProvider = ({ children }: { children: React.ReactNode }) => {
@@ -42,12 +48,42 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
     });
   });
   }
+
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts(prevProducts => {
+      return prevProducts.map(prevProduct => {
+        if (prevProduct.id === productId) {
+          return { ...prevProduct, quantity: prevProduct.quantity - 1 };
+        }
+        return prevProduct;
+      });
+    });
+  }
+
+  const increaseProductQuantity = (productId: string) => {
+    setProducts(prevProducts => {
+      return prevProducts.map(prevProduct => {
+        if (prevProduct.id === productId) {
+          return { ...prevProduct, quantity: prevProduct.quantity + 1 };
+        }
+        return prevProduct;
+      });
+    });
+  }
+
+  const removeProduct = (productId: string) => {
+    setProducts(prevProducts => prevProducts.filter(product => product.id !== productId));
+  }
+
   return (
     <CartContext.Provider value={{
       isOpen,
       products,
       toggleCart,
       addProduct,
+      decreaseProductQuantity,
+      increaseProductQuantity,
+      removeProduct,
     }}>
       {children}
     </CartContext.Provider>
